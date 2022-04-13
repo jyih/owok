002bb8ed0dfa741b8ee02f01cc0702d8813570b2
Sharon Fang <sharonfang8@gmail.com>
2022-04-13 11:17:09 -0700

    added commands.txt and finished models

---
+++ app/models/comments.py
@@ -0,0 +1,34 @@
+from .db import db
+from sqlalchemy import DateTime
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    game_id = db.Column(
+        db.Integer, db.ForeignKey('games.id'), nullable=False)
+    player_id = db.Column(
+        db.Integer, db.ForeignKey('users.id'), nullable=False)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    content = db.Column(db.String(12000), nullable=False)
+    created_at = db.Column(DateTime())
+    updated_at = db.Column(DateTime())
+
+    user = db.relationship('User', back_populates='comments')
+
+    game = db.relationship('Game', back_populates='comments')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'game_id': self.game_id,
+            'player_id': self.player_id,
+            'username': self.username,
+            'content': self.content,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
+
+    def get_user(self):
+        return self.user.to_dict()
