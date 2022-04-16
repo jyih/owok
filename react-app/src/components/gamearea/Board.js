bbcd598417d51607d9879d46369f1a6b9f879b8f
jojo <johnathanyih@gmail.com>
2022-04-15 17:52:32 -0700

    octo-directional 5 piece check

--- react-app/src/components/gamearea/Board.js
+++ react-app/src/components/gamearea/Board.js
@@ -21,9 +21,23 @@ const Board = () => {
   // with map of vectors added to indecies of the board grid, and then do the test. return number
   // of neighbors and do this (after doing second paragraph)
 
-  let currPiece = omok_piece_mushroom;
-  let oppPiece = omok_piece_slime;
-  const moves = [];
+  let currPiece = 'mushroom';
+  let oppPiece = 'slime';
+  let gameStatus = null;
+  let lastMove = null;
+  const notation = [];
+  const pieces = {
+    mushroom: omok_piece_mushroom,
+    slime: omok_piece_slime
+  }
+  const board = {};
+  const move = {
+    up: -100,
+    down: 100,
+    left: -1,
+    right: 1,
+  }
+  // const board = Array(15*15).fill('');
 
   const placePiece = (e) => {
     // let r = coord.slice(0, 2)
@@ -36,26 +50,150 @@ const Board = () => {
       let piece = document.createElement('img')
       // change style background image to the img (might be better for performance)
       // bc not adding nodes to dom, just updating the node's style
-      piece.src = currPiece
+      piece.src = pieces[currPiece]
       square.appendChild(piece)
-      moves.push(`${e.target.id.slice(0,2)}${e.target.id.slice(-2)}`)
-      console.log('moves',moves)
+
+      let noHyphen = `${e.target.id.slice(0, 2)}${e.target.id.slice(-2)}`;
+      lastMove = parseInt(noHyphen);
+
+      notation.push(noHyphen)
+      board[lastMove] = currPiece;
+
+      console.log('moves:', notation)
+      console.log('board:', board)
       swapPiece()
+      checkGame();
+      console.log('gameStatus:', gameStatus)
     }
   }
 
   const swapPiece = () => {
     console.log("Click!");
-    if (currPiece === omok_piece_mushroom) {
-      console.log("click 1");
-      currPiece = omok_piece_slime;
-      oppPiece = omok_piece_mushroom;
-    } else {
-      console.log("click 2");
-      currPiece = omok_piece_mushroom;
-      oppPiece = omok_piece_slime;
-    }
+    let temp = currPiece;
+    currPiece = oppPiece;
+    oppPiece = temp;
+    // if (currPiece === 'mushroom') {
+    //   console.log("click 1");
+    //   currPiece = omok_piece_slime;
+    //   oppPiece = omok_piece_mushroom;
+    // } else {
+    //   console.log("click 2");
+    //   currPiece = omok_piece_mushroom;
+    //   oppPiece = omok_piece_slime;
+    // }
   };
+
+  const checkGame = () => {
+    let lastPiece = board[lastMove];
+    console.log('lastmove:', lastMove)
+    console.log('piece logic check:', lastPiece === oppPiece)
+
+    //check vertical
+    let countNS = 1;
+    //check up
+    let countN = 0;
+    let lookMove = lastMove + move.up;
+    let lookPiece = board[lookMove];
+    while (lookPiece === lastPiece && countN < 4 && countNS < 5) {
+      countN++;
+      lookMove += move.up;
+      lookPiece = board[lookMove];
+    }
+
+    countNS += countN;
+
+    //check down
+    let countS = 0;
+    lookMove = lastMove + move.down;
+    lookPiece = board[lookMove];
+    while (lookPiece === lastPiece && countS < 4 && countNS < 5) {
+      countS++;
+      lookMove += move.down;
+      lookPiece = board[lookMove];
+    }
+
+    countNS += countS;
+
+    //check horizontal
+    let countWE = 1;
+    //check left
+    let countW = 0;
+    lookMove = lastMove + move.left;
+    lookPiece = board[lookMove];
+    while (lookPiece === lastPiece && countW < 4 && countWE < 5) {
+      countW++;
+      lookMove += move.left;
+      lookPiece = board[lookMove];
+    }
+    countWE += countW
+
+    //check right
+    let countE = 0;
+    lookMove = lastMove + move.right;
+    lookPiece = board[lookMove];
+    while (lookPiece === lastPiece && countE < 4 && countWE < 5) {
+      countE++;
+      lookMove += move.right;
+      lookPiece = board[lookMove];
+    }
+    countWE += countE;
+
+    //check NESW diag
+    let countNESW = 1;
+    //check top right
+    let countNE = 0;
+    lookMove = lastMove + move.right + move.up;
+    lookPiece = board[lookMove];
+    while (lookPiece === lastPiece && countNE < 4 && countNESW < 5) {
+      countNE++;
+      lookMove = lookMove + move.right + move.up;
+      lookPiece = board[lookMove];
+    }
+    countNESW += countNE
+
+    //check bot left
+    let countSW = 0;
+    lookMove = lastMove + move.left + move.down;
+    lookPiece = board[lookMove];
+    while (lookPiece === lastPiece && countSW < 4 && countNESW < 5) {
+      countSW++;
+      lookMove = lookMove + move.left + move.down;
+      lookPiece = board[lookMove];
+    }
+    countNESW += countSW;
+
+    //check NESW diag
+    let countNWSE = 1;
+    //check top right
+    let countNW = 0;
+    lookMove = lastMove + move.left + move.up;
+    lookPiece = board[lookMove];
+    while (lookPiece === lastPiece && countNW < 4 && countNWSE < 5) {
+      countNW++;
+      lookMove = lookMove + move.left + move.up;
+      lookPiece = board[lookMove];
+    }
+    countNWSE += countNW
+
+    //check bot left
+    let countSE = 0;
+    lookMove = lastMove + move.right + move.down;
+    lookPiece = board[lookMove];
+    while (lookPiece === lastPiece && countSE < 4 && countNWSE < 5) {
+      countSE++;
+      lookMove = lookMove + move.right + move.down;
+      lookPiece = board[lookMove];
+    }
+    countNWSE += countSE;
+
+
+    if (
+      countNS >= 5 ||
+      countWE >= 5 ||
+      countNESW >= 5 ||
+      countNWSE >= 5
+    ) gameStatus = lastPiece;
+  }
 
   return (
     <div className="board_container">
