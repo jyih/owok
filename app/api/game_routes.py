4b540b1883f26ecf5f6266b9cf24c705e5f42076
jojo <johnathanyih@gmail.com>
2022-04-23 15:55:31 -0700

    pre-test game_routes done

--- app/api/game_routes.py
+++ app/api/game_routes.py
@@ -2,98 +2,124 @@ from flask import Blueprint, request
 from flask_login import login_required
 from app.models import db, Game, User
 
-game_routes = Blueprint('games', __name__)
+game_routes = Blueprint("games", __name__)
 
-@game_routes.route('/')
+
+@game_routes.route("/")
 def all_games():
-  games = Game.query.all()
-  return {game.to_dict()['id']:game.to_dict() for game in games}
+    games = Game.query.all()
+    return {game.to_dict()["id"]: game.to_dict() for game in games}
 
-@game_routes.route('/<int:id>')
+
+@game_routes.route("/<int:id>")
 def get_game(id):
-  #Get specific game via id
-  game = Game.query.get(id)
-  print('game:', game)
-  if not game:
-    return {'errors': 'Game not found'}
-  return {**game.to_dict()}
+    # Get specific game via id
+    game = Game.query.get(id)
+    print("game:", game)
+    if not game:
+        return {"errors": "Game not found"}
+    return {**game.to_dict()}
 
-@game_routes.route('/', methods=['POST'])
+
+@game_routes.route("/", methods=["POST"])
 # @login_required
 def create_game():
-  data = request.json
-  print(f'''
+    data = request.json
+    print(
+        f"""
   SHOW ME THE DATA {data}
-  ''')
-  game = Game(
-      player_one_id = data['player_one_id'],
-      player_two_id = data['player_two_id'],
+  """
+    )
+    game = Game(
+        player_one_id=data["player_one_id"],
+        player_two_id=data["player_two_id"],
+    )
+    db.session.add(game)
 
-  )
-  db.session.add(game)
+    db.session.commit()
+    return game.to_dict()
 
-  db.session.commit()
-  return game.to_dict()
 
-@game_routes.route('/<int:id>', methods=['PATCH'])
+@game_routes.route("/<int:id>", methods=["PUT"])
+# @login_required
 def update_game(id):
-  data = request.json
-  move = data['move']
-  game = Game.query.get(id)
-  if not game:
-    return {'errors': 'Game not found'}
-  place_piece(game, move)
-  db.session.commit()
-  return {**game.to_dict()}
+    data = request.json
+    move = data["move"]
+    game = Game.query.get(id)
+    if not game:
+        return {"errors": "Game not found"}
+    else:
+        print(
+            f"""
+    PLACE PIECE:
+    {type(game.board)}
+    """
+        )
+
+        place_piece(game, move)
+        return {**game.to_dict()}
+
 
 # ==========
 # Game Logic
 # ==========
 
 displace = {
-    'up': -100,
-    'down': 100,
-    'left': -1,
-    'right': 1,
+    "up": -100,
+    "down": 100,
+    "left": -1,
+    "right": 1,
 }
 
+
 def place_piece(game, move):
-  if not game.board[move]:
-    game.board[move] = game.turn
-    check_game(game, move)
+    if move not in game.board:
+        game.board[move] = game.turn
+        game.moves += f",{move}" if games.moves.length >= 0 else f"{move}"
+        check_game(game, move)
+
 
 def check_game(game, move, n=5):
-  if game.winner_id is not None:
-    vertical = check_line(game, move, displace.up, n)
-    horizontal = check_line(game, move, displace.right, n)
-    forward_diag = check_line(game,move, displace.up + displace.right, n)
-    backward_diag = check_line(game, move, displace.up + displace.left, n)
+    if game.winner_id is not None:
+        vertical = check_line(game, move, displace.up, n)
+        horizontal = check_line(game, move, displace.right, n)
+        forward_diag = check_line(game, move, displace.up + displace.right, n)
+        backward_diag = check_line(game, move, displace.up + displace.left, n)
 
-    if (vertical >= n or horizontal >= n or forward_diag >= n or backward_diag >= n):
-      end_game(game)
-    else:
-      swap_piece(game)
+        if vertical >= n or horizontal >= n or forward_diag >= n or backward_diag >= n:
+            end_game(game)
+        else:
+            swap_piece(game)
+
 
 def check_line(game, move, displacement, n=5):
-  return check_vector(game, move, displacement, n) + checkVector(game, move, -displacement, n) -1
+    return (
+        check_vector(game, move, displacement, n)
+        + checkVector(game, move, -displacement, n)
+        - 1
+    )
+
 
 def check_vector(game, move, displacement, n=5):
-  look_piece = game.board[move]
-  count = 0
+    look_piece = game.board[move]
+    count = 0
 
-  while game.board[move] and look_piece == game.board[move] and count < n:
-    count += 1
-    look_move = move + (displacement * count)
-    look_piece = game.board[look_move]
+    while game.board[move] and look_piece == game.board[move] and count < n:
+        count += 1
+        look_move = move + (displacement * count)
+        look_piece = game.board[look_move]
 
-  return count
+    return count
+
 
 def end_game(game):
-  if game.winner_id is not None:
-    if game.moves.split(',').length == 225:
-      game.winner_id = -1 #tie
-    else:
-      game.winner_id = game.turn
+    if game.winner_id is not None:
+        if game.moves.split(",").length == 225:
+            game.winner_id = -1  # tie
+        else:
+            game.winner_id = game.turn
+    db.session.commit()
+
 
 def swap_piece(game, players=2):
-  game.turn = (game.turn + 1) % players
+    game.turn = (game.turn + 1) % players
